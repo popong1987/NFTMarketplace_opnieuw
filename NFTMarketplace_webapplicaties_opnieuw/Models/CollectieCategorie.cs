95fee0f37eed8a897c19873b75b1fc9adf8ea9c7
liesbeth1987 <r0888657@student.thomasmore.be>
2022-12-08 09:41:50 +0100

    collectiecategorie en collectiecreator models aanmaken

---
+++ NFTMarketplace_webapplicaties_opnieuw/Models/CollectieCategorie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NFTMarketplace_webapplicaties_opnieuw.Models
+{
+    public class CollectieCategorie
+    {
+        public int CollectieCategorieId { get; set; }
+
+
+        [Required]
+        public int CollectieId { get; set; }
+
+        [Required]
+        public int CategorieId { get; set; }
+
+        //navigation properties
+        public Collectie Collectie { get; set; }
+        public Categorie Categorie { get; set; }
+    }
+}
