eb13ea70c6e8aa44cf7a4a3c848f3dd4150e288a
liesbeth1987 <r0888657@student.thomasmore.be>
2022-12-08 09:43:31 +0100

    creator en order models aanmaken

---
+++ NFTMarketplace_webapplicaties_opnieuw/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NFTMarketplace_webapplicaties_opnieuw.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [Required]
+        public decimal TotalePrijs { get; set; }
+
+
+        public int GebruikerId { get; set; }
+
+        //navigation properties
+
+        public List<OrderProduct> OrderProducten { get; set; }
+        /*public Gebruiker Gebruiker { get; set; }*/
+    }
+}
