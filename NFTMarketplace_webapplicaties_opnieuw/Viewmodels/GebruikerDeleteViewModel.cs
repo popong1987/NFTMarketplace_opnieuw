b41731ca1df7016603efaae96f042d9deeb52595
liesbeth1987 <r0888657@student.thomasmore.be>
2022-12-10 15:20:51 +0100

    delete user en edit user verbeterd

---
+++ NFTMarketplace_webapplicaties_opnieuw/Viewmodels/GebruikerDeleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NFTMarketplace_webapplicaties_opnieuw.Models;
+using System;
+using System.Collections.Generic;
+
+namespace NFTMarketplace_webapplicaties_opnieuw.Viewmodels
+{
+    public class GebruikerDeleteViewModel
+    {
+        public string GebruikerId { get; set; }
+        public string Naam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ NFTMarketplace_webapplicaties_opnieuw/Viewmodels/GebruikerEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace NFTMarketplace_webapplicaties_opnieuw.Viewmodels
+{
+    public class GebruikerEditViewModel
+    {
+        public string GebruikerId { get; set; }
+        [Required]
+        public string Naam { get; set; }
+        [Required]
+
+        public DateTime Geboortedatum { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+
+    }
+}
