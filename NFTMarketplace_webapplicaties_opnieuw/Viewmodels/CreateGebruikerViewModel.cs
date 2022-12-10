ef11ff0fa8e70b21ef8935180379c3241dd246d1
liesbeth1987 <r0888657@student.thomasmore.be>
2022-12-10 11:13:40 +0100

    userroles geimplementeerd

---
+++ NFTMarketplace_webapplicaties_opnieuw/Viewmodels/CreateGebruikerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NFTMarketplace_webapplicaties_opnieuw.Viewmodels
+{
+    public class CreateGebruikerViewModel
+    {
+        public string Naam { get; set; }
+        public string UserName { get; set; }
+        public DateTime GeboorteDatum { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ NFTMarketplace_webapplicaties_opnieuw/Viewmodels/GrantPermissionsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NFTMarketplace_webapplicaties_opnieuw.Viewmodels
+{
+    public class GrantPermissionsViewModel
+    {
+        public SelectList Gebruikers { get; set; }
+        public SelectList Rollen { get; set; }
+        public string GebruikerId { get; set; }
+        public string RolId { get; set; }
+    }
+}
